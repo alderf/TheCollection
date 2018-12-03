9a26151cf0bfe44f7431c2a357292b6435604973
Charlotte Allen <takev.takev@gmail.com>
2018-12-02 20:03:19 -0800

    Modified project and removed top src

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject thecollective "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]
+                 [ring/ring-json "0.1.2"]
+                 [c3p0/c3p0 "0.9.1.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.168"]
+                 [cheshire "4.0.3"]]
+  :plugins [[lein-ring "0.7.3"]]
+  :ring {:handler clojure-rest.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
